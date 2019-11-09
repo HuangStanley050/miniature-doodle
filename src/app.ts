57d8073cdafba23dfed0907091a11e617d86f05d
Stanley Huang <huang.stanley050@gmail.com>
2019-11-09 20:56:31 +1100

    app created, but need definition for controller in constructor

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,26 @@
-import express, { Request, Response } from "express";
-import authRouter from "./routes/auth";
-import cors from "cors";
+import * as express from "express";
+
+export class App {
+  public app: express.Application;
+  public port: number;
+
+  constructor(controllers, port) {
+    this.app = express();
+    this.port = port;
+    this.initializeMiddleware();
+    this.intializeControllers(controllers);
+  }
+  private initializeMiddleware = () => {
+    this.app.use(express.json());
+  };
+  private intializeControllers = controllers => {
+    controllers.forEach(controller => {
+      this.app.use("/", controller.router);
+    });
+  };
+  public listen(): void {
+    this.app.listen(this.port, () => {
+      console.log(`server running on ${port}`);
+    });
+  }
+}
