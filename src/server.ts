39a4343238cd9ba26779e339e03488b7f9e5e9b7
Stanley Huang <huang.stanley050@gmail.com>
2019-11-13 21:47:51 +1100

    still fixing issue

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import { App } from "./App";
 import AuthController from "./controllers/Auth";
+import mongoose from "mongoose";
 const authController = new AuthController();
+
 declare var process: {
   env: {
     PORT: number;
   };
 };
+const mongoURI =
+  "mongodb://heroku_zgs22sg7:5b8hv4cl7p45sn67jq43v1sc5c@ds029817.mlab.com:29817/heroku_zgs22sg7";
 const port: number = process.env.PORT || 3000;
 const app = new App(port, [authController]);
 
-app.listen();
+mongoose.connect(mongoURI).then(() => {
+  app.listen();
+});
